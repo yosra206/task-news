e78750becd56c92e4522d616c24c6553e5837954
yosra206 <71187512+yosra206@users.noreply.github.com>
2022-04-18 22:23:50 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
-
+const request = require('request')
 const path = require('path')
 const hbs = require('hbs')
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 9000;
 const viewsPath = path.join(__dirname, './views')
 
 app.use(express.static(viewsPath))
@@ -13,30 +13,19 @@ app.set('views', viewsPath)
 
 
 
-// app.get('/indexUrl', (req, res) => {
-//     res.render('index', {
-//         title: req.query.articles.title,
-//         description: req.query.articles.description,
-//         img: req.query.articles.img
-
-//     })
-// })
-
-const news = require('./tools/news')
-app.get('/news', (req, res) => {
-    if (error) {
-        res.send(error)
-    } else {
-        res.send({
-            // news: req.query.articles,
-            title: req.query.articles.title,
-            description: req.query.articles.description,
-            img: req.query.articles.img
-
+app.get('/', (req, res) => {
+    const url = 'https://newsapi.org/v2/top-headlines?country=eg&category=entertainment&apiKey=702e9ad75b684b2ba13de7a0b5cf6ba3'
+    request({ url, json: true }, (error, response) => {
+        //console.log(response.body.articles)
+        res.render('news', {
+            title: 'All news',
+            data: response.body.articles
         })
-    }
-
+        console.log(data)
+    })
 })
+
+
 app.listen(port, () => {
     console.log("server  is running", port)
 })
